f4a5ae2932af3962732d4dcd2c4e86fb9e7db206
Mukesh Suthar <msuthar4y@gmail.com>
2021-12-04 13:12:11 +0530

    login page completed

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,11 @@
 import Head from "next/head";
+import {
+  FaFacebookF,
+  FaLinkedin,
+  FaGoogle,
+  FaRegEnvelope,
+} from "react-icons/fa";
+import { MdLockOutline } from "react-icons/md";
 
 export default function Home() {
   return (
@@ -19,6 +26,79 @@ export default function Home() {
                 Sign in to Account
               </h2>
               <div className="border-2 w-10 border-green-500 inline-block mb-2"></div>
+              <div className="flex justify-center my-2">
+                <a
+                  href="#"
+                  className="border-2 border-gray-200
+                rounded-full p-3 mx-1"
+                >
+                  <FaFacebookF className="text-sm" />
+                </a>
+                <a
+                  href="#"
+                  className="border-2 border-gray-200
+                rounded-full p-3 mx-1"
+                >
+                  <FaLinkedin className="text-sm" />
+                </a>
+                <a
+                  href="#"
+                  className="border-2 border-gray-200
+                rounded-full p-3 mx-1"
+                >
+                  <FaGoogle className="text-sm" />
+                </a>
+              </div>
+              <p className="text-grey-400 my-3">Or use your email account</p>
+              <div className="flex flex-col items-center ">
+                <div
+                  className="bg-gray-200 w-64 p-2
+                  rounded-sm
+                flex items-center mb-2"
+                >
+                  <FaRegEnvelope className="text-grey-400 m-2" />
+                  <input
+                    className="bg-gray-200 outline-none flex-1 text-sm"
+                    type="email"
+                    name="email"
+                    placeholder="Email Address"
+                  />
+                </div>
+                <div
+                  className="bg-gray-200 w-64 p-2
+                  rounded-sm
+                flex items-center mb-2"
+                >
+                  <MdLockOutline className="text-grey-400 m-2" />
+                  <input
+                    className="bg-gray-200 outline-none flex-1 text-sm"
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                  />
+                </div>
+                <div className="flex justify-between w-64 mb-5">
+                  <label className="flex items-center text-xs">
+                    <input
+                      type="checkbox"
+                      name="remember"
+                      id="rememberCheckbox"
+                      className="mr-1"
+                    />
+                    Remember me
+                  </label>
+                  <a href="#" className="text-xs">
+                    Forgot password?
+                  </a>
+                </div>
+                <a
+                  className="border-2 font-semibold text-green-500 border-green-500 rounded-full px-12 py-2 inline-block
+              hover:bg-green-500 hover:text-white transition-all"
+                  href="#"
+                >
+                  Sign in
+                </a>
+              </div>
             </div>
           </div>
           <div className="w-2/5 bg-green-500 py-36 px-12 text-white rounded-tr-2xl rounded-br-2xl">
